7326aa2b7ba6fc22893827fb754262d82b0c5cd9
Jebakani <vjishwarya@gmail.com>
2021-06-28 18:12:10 +0530

    [Jebakani] SnakeAndLadderProblem

---
+++ SnakeAndLadderComputation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadderComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder World");
+            Console.Read();
+        }
+    }
+}
